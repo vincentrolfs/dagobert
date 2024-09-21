ed074b2dc8b49003d195ce37e576600ad615b998
Vincent Rolfs <mail@vincentrolfs.dev>
2024-09-21 23:37:10 +0300

    Change DATE_START

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -7,5 +7,5 @@ export const currencyFactors: Record<Currency, number> = {
   TRY: 0.0262674,
 };
 
-export const DATE_START = "2024-09-18";
+export const DATE_START = "2024-09-21";
 export const BUDGET_PER_DAY = 25;
